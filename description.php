2112e1db7eb472cd69f2c8ae1d28c4a6ad66091e
Aleksey Kopasov <alesey.k1@gmail.com>
2023-03-09 23:39:10 +0300

    initial commit

---
+++ description.php
@@ -0,0 +1,8 @@
+<?
+$arTemplate = array (
+  'NAME' => 'Шаблон Gluttonous',
+  'DESCRIPTION' => 'Интеграция лендинга Gluttonous на Bitrix',
+  'SORT' => 300,
+  'TYPE' => '',
+);
+?>
---
+++ header.php
@@ -0,0 +1,14 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); 
+
+use Bitrix\Main\Page\Asset;
+
+$asset = Asset::getInstance();
+
+// $asset->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
+// $asset->addString('<link rel="preload" href="' . SITE_TEMPLATE_PATH . '/assets/fonts/montserrat-regular.woff2" as="font"
+// type="font/woff2" crossorigin="anonymous">');
+// $asset->addString('<link rel="icon" href="' . SITE_TEMPLATE_PATH . '/assets/images/favicon.ico" type="image/x-icon" />');
+// $asset->addString('<link rel="shortcut icon" href="' . SITE_TEMPLATE_PATH . '/assets/images/favicon.ico" type="image/x-icon" />');
+?>
+
